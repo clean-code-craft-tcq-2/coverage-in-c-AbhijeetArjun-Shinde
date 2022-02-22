9c3e7b7689a6b2a8cda4d627cc1a48f8d555f1f3
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-02-22 09:25:54 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -8,3 +8,20 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(60, 20, 30) == TOO_HIGH);
   REQUIRE(inferBreach(27, 20, 30) == NORMAL);
 }
+
+
+TEST_CASE("infers breach according to cooling type") {
+  Limits TempLimit[NO_COOLING_TYPES]= {{PASSIVE_COOLING_MIN_LIMIT,PASSIVE_COOLING_MAX_LIMIT},
+                                 {HI_ACTIVE_COOLING_MIN_LIMIT,HI_ACTIVE_COOLING_MAX_LIMIT},
+                                 {MED_ACTIVE_COOLING_MIN_LIMIT,MED_ACTIVE_COOLING_MAX_LIMIT}};
+  
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5, TempLimit) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 27, TempLimit) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 37, TempLimit) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -5, TempLimit) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,27 , TempLimit) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 47, TempLimit) == TOO_HIGH); 
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -5, TempLimit) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 27, TempLimit) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 42, TempLimit) == TOO_HIGH);
+}
