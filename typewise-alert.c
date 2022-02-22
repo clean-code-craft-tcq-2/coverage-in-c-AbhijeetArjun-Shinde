8e58bfea40dd56854669055a89110d90884d8477
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-02-22 08:55:33 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -12,43 +12,41 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
   int lowerLimit = TempLimit[coolingType].Min_Limit;
   int upperLimit = TempLimit[coolingType].Max_Limit;
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
-void PrintOnConsole(char message[]){
+
+void PrintOnConsole(char *message){
   printf(" %s!\n", message); 
   return;
 }
 
-
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
-  void (*FnPtrPrinter) (char message[]) ;
+  void (*FnPtrPrinter) (char *message) ;
   FnPtrPrinter = &PrintOnConsole;
   switch(alertTarget) {
     case TO_CONTROLLER:
-      sendToController(breachType);
+      sendToController(breachType, FnPtrPrinter);
       break;
     case TO_EMAIL:
-      sendToEmail(breachType , FnPtrPrinter );
+      sendToEmail(breachType , FnPtrPrinter);
       break;
   }
 }
 
-void sendToController(BreachType breachType) {
+void sendToController(BreachType breachType,void (*FnPtrPrinter) (char *message)) {
   const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
+  char *PrintMessage;
+  sprintf(PrintMessage,"%x : %x\n", header, breachType);
+  FnPtrPrinter(PrintMessage)
 }
 
-void sendToEmail(BreachType breachType , void (*FnPtrPrinter) (char message[]) ) {
-  char recepientEmail[] = "a.b@c.com";
-  char msg[]= "To: ";
-  strcat(msg, recepientEmail);
-  strcat(msg , AlertMessage[breachType]);
-  FnPtrPrinter(msg);
+void sendToEmail(BreachType breachType , void (*FnPtrPrinter) (char *message) ) {
+  const char *recepientEmail = "a.b@c.com";
+  char *PrintMessage;
+  sprintf(PrintMessage,"To : %s\n", recepientEmail);
+  FnPtrPrinter(PrintMessage);
 }
